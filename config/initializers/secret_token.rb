202f9179a8e8e436969050da89503be77743c3bd
David Allen <cavepaintings@gmail.com>
2011-07-17 19:12:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoodleLocation::Application.config.secret_token = '160e50940b2d919855edf414c00bbeb1cf2cddf4f6f435b23ec555afb19bdd6d23b10dd72a3ed5f6af1307302b7fc1d26bd52bd3e24b7cc77b6844e47e24a784'
