b79feb756751b27e2d5600ae3cda9b19613ba1d4
David Allen <cavepaintings@gmail.com>
2011-07-17 22:46:43 -0700

    Changed basis of app from posts to users.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,46 +1,18 @@
 class PostsController < ApplicationController
 	
 	def index
-		list
-		render('list')
 	end
 	
 	def list
-		@posts = Post.order('id DESC')
-	end
-	
-	def show
-		@post = Post.find(params[:id])
-	end
-	
-	def new
-		@post = Post.new
 	end
 	
 	def create
-		@post = Post.new(params[:post])
-		
-		if @post.save
-			flash[:notice] = "You have just made a post!"
-			redirect_to(:action => 'list')
-		else
-			render('new')
-		end
 	end
 	
 	def edit
-		@post = Post.find(params[:id])
 	end
 	
 	def update
-		@post = Post.find(params[:id])
-		
-		if @post.update_attributes(params[:post])
-			flash[:notice] = "You have just updated your post!"
-			redirect_to(:action => 'list')
-		else
-			render('edit')
-		end
 	end
 	
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+	
+	def index
+		@user = User.new
+	end
+	
+	def list
+		@users = User.order('id DESC')
+	end
+	
+	def create
+		@user = User.new(params[:user])
+		
+		if @user.save
+			flash[:notice] = "You have just made a post!"
+			redirect_to(:action => 'list')
+		else
+			render('new')
+		end
+	end
+	
+	def edit
+		@user = User.find(params[:id])
+	end
+	
+	def update
+		@user = User.find(params[:id])
+		
+		if @user.update_attributes(params[:user])
+			flash[:notice] = "You have just updated your post!"
+			redirect_to(:action => 'list')
+		else
+			render('edit')
+		end
+	end
+	
 end
