235456f4b296f130e620057a0a837d5d42a802d5
David Allen <cavepaintings@gmail.com>
2011-07-18 16:59:56 -0700

    Made it look more app like!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 NoodleLocation::Application.routes.draw do
+	
+  root :to => "users#index"
+  match "/messages", :to => "users#list"
+  match ':controller(/:action(/:id(.:format)))'
+  	
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,11 +53,11 @@ NoodleLocation::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "users#index"
+  # root :to => "users#index"
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id(.:format)))'
+  # match ':controller(/:action(/:id(.:format)))'
 end
