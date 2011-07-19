224cf7d306bfe3e7a67054940b707b317de9fbe2
David Allen <cavepaintings@gmail.com>
2011-07-19 01:46:48 -0700

    Map bug.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 	
-	acts_as_gmappable
+	acts_as_gmappable :lat => 'latitude', :lng => 'longitude', :address => "address"
 	
 	belongs_to :user
 	validates_presence_of :content
